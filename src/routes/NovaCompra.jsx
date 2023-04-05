8b874bd4cd86f59a56dfeb6175c7337954d4baa9
unknown <samuelvilas.28@gmail.com>
2023-04-05 19:41:16 -0300

    Alteração de estilo

--- src/routes/NovaCompra.jsx
+++ src/routes/NovaCompra.jsx
@@ -3,26 +3,43 @@ import './NovaCompra.css';
 
 function NovaCompra(){
     return(
-        <div>
+        <div className="containerForm">
             <h1>Cadastrar Compra</h1>
             <form className="formNovaCompra">
-                <label for="cliente">Nome Cliente</label>
-                <input type="text" placeholder="Cliente" id="cliente"/>
-                <label for="ID">ID do produto</label>
-                <input type="text" placeholder="ID" id="ID"/>
-                <label for="material">Material</label>
-                <select id="material" name="material">
-                    <option value="Ferro">Ferro</option>
-                    <option value="Plastico">Plastico</option>
-                    <option value="Papel">Papel</option>
-                </select>
-                <label for="qtd">Quantidade</label>
-                <input type="text" placeholder="Qtd." id="qtd"/>
-                <label for="valorKg">Valor unitário</label>
-                <input type="text" placeholder="ValorKg" id="total"/>
-                Valor Total
-                <input type="text" placeholder="Total" id="total"/>
-                <input type="button" value="Cadastrar" id="cadastrar" />
+                {/* <h3>Insira os dados da compra<br/>Ao finalizar clique em cadastrar ou em + para adicionar novos itens</h3> */}
+                <div className="divInput">
+                    <label for="cliente">Nome Cliente</label>
+                    <input type="text" placeholder="Cliente" id="cliente"/>
+                </div>
+                <div className="divInput">
+                    <label for="ID">ID do produto</label>
+                    <input type="text" placeholder="ID" id="ID"/>
+                </div>
+                <div className="divInput">
+                    <label for="material">Material</label>
+                    <select id="material" name="material">
+                        <option value="Ferro">Ferro</option>
+                        <option value="Plastico">Plastico</option>
+                        <option value="Papel">Papel</option>
+                    </select>
+                </div>
+                <div className="divInput">
+                    <label for="qtd">Quantidade</label>
+                    <input type="text" placeholder="Qtd." id="qtd"/>
+                </div>
+                <div className="divInput">
+                    <label for="valorKg">Valor unitário</label>
+                    <input type="text" placeholder="ValorKg" id="total"/>
+                </div>
+                <div className="divInput">
+                    <label for="Total">Valor Total</label>
+                    <input type="text" placeholder="Total" id="total"/>
+                </div>
+                <div className="divInput">
+                    <button id="btnCad">Cadastrar</button>
+                    <button id="btnAdd">+</button>
+                </div>
+                
             </form>
         </div>
     )
